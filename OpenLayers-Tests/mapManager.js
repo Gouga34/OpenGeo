2b32b808c8ead328434cabbdebdcac00bb586bed
Gouga34 <morgane.vidal@wanadoo.fr>
2016-03-04 10:29:07 +0100

    Map qui affiche un fond de carte (pays), les parcelles et un pt on click

---
+++ OpenLayers-Tests/mapManager.js
@@ -0,0 +1,55 @@
+function getView() {
+  return new ol.View({
+    center: MAP_CENTER,
+    zoom: MAP_ZOOM
+  });
+}
+
+function getViewControls() {
+  return ol.control.defaults({
+    attributionOptions: {
+      collapsible: false
+    }
+  });
+}
+
+function getViewLayers() {
+  layers =  [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    }),
+    new ol.layer.Tile({
+          source: new ol.source.TileWMS({
+            projection: MAP_PROJECTION,
+            url: WMS_SERVICE_URL,
+            params: {
+              'LAYERS': WMS_SERVICE_LAYERS
+            }
+          })
+        })
+  ];
+  return layers;
+}
+
+function generateAndGetMap() {
+  view = getView();
+  controls = getViewControls();
+  layers = getViewLayers();
+
+  var map = new ol.Map({
+    target: 'map',
+    layers: layers,
+    view: view,
+    controls: controls
+  });
+
+  return map;
+}
+
+function getVectorLayer(startPoint, destPoint) {
+  return new ol.layer.Vector({
+    source: new ol.source.Vector({
+      features: [startPoint, destPoint]
+    })
+  });
+}
