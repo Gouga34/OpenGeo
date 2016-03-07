c47cfc95e751ae9c3375f224d80a32440fc849ed
Gouga34 <morgane.vidal@wanadoo.fr>
2016-03-07 14:07:52 +0100

    Update du client avec choix de réception des données en JSON ou non

---
+++ OpenLayers-Tests/mapWithInfoOnClick/client/JS/constants.js
@@ -0,0 +1,19 @@
+const SERVER_TYPE = 'geoserver';
+
+const WMS_SERVICE_URL = 'http://localhost:8080/geoserver/wms';
+const WMS_SERVICE_LAYERS = 'nyc:parcelles';
+
+const SERVER_URL = 'http://localhost/OpenGeo/OpenLayers-Tests/mapWithInfoOnClick/server/';
+
+const MAP_PROJECTION = 'EPSG:4171'; //data source projection
+const MAP_CENTER = [-112.50000000000003, 163.33333333333326];
+const MAP_ZOOM = 10;
+
+const PNG_FORMAT = 'image/png';
+const JSON_FORMAT = 'application/json';
+const HTML_FORMAT = 'text/html';
+const TEXT_FORMAT = 'text/plain';
+
+const INFO_FORMAT = JSON_FORMAT;
+const FORMAT = PNG_FORMAT;
+const CRS = "EPSG:3857";
---
+++ OpenLayers-Tests/mapWithInfoOnClick/client/JS/mapManager.js
@@ -0,0 +1,98 @@
+
+function createView() {
+  return new ol.View({
+    center: MAP_CENTER,
+    zoom: MAP_ZOOM
+  });
+}
+
+function getWMSSource() {
+  return new ol.source.ImageWMS({
+    url: WMS_SERVICE_URL,
+    params: {'LAYERS': WMS_SERVICE_LAYERS},
+    serverType: SERVER_TYPE,
+    crossOrigin: null
+  });
+}
+
+function getWMSLayer(wmsSource) {
+  return new ol.layer.Image({
+    source: wmsSource
+  });
+}
+
+function generateAndGetMap(view, wmsLayer) {
+  return new ol.Map({
+    layers: [
+              new ol.layer.Tile({
+                source: new ol.source.OSM()
+              }),
+              wmsLayer
+            ],
+    target: 'map',
+    view: view
+  });
+}
+
+function getRequestUrl(wmsSource, evt, viewResolution) {
+  if (INFO_FORMAT == JSON_FORMAT) {
+    var url = SERVER_URL
+            + "?FORMAT=" + FORMAT
+            + "&QUERY_LAYERS=" + WMS_SERVICE_LAYERS
+            + "&LAYERS=" + WMS_SERVICE_LAYERS
+            + "&INFO_FORMAT=" + INFO_FORMAT
+            + "&CRS=" + CRS;
+  } else if (INFO_FORMAT == HTML_FORMAT || INFO_FORMAT == TEXT_FORMAT) {
+    var url = wmsSource.getGetFeatureInfoUrl(
+        evt.coordinate, viewResolution, 'EPSG:3857',
+        {'INFO_FORMAT': INFO_FORMAT});
+  }
+  return url;
+}
+
+function getAndPrintJSONData(url) {
+  $.get(
+    url,
+      // success callback
+    function (data) {
+          var elementData = JSON.parse(data);
+
+          var dataToPrint = "<table class=\"table\">"
+                            + "<tr>"
+                              + "<th> id </th>"
+                              + "<th> name </th>"
+                              + "<th> type </th>"
+                            + "</tr>"
+                            + "<tr>"
+                              + "<td>" + elementData.features[0].properties.id + "</td>"
+                              + "<td>" + elementData.features[0].properties.nom + "</td>"
+                              + "<td>" + elementData.features[0].properties.type + "</td>"
+                              + "</tr>"
+                          + "</table>";
+
+          document.getElementById('info').innerHTML = dataToPrint;
+    }
+  );
+}
+
+function getAndPrintTextData(url) {
+  document.getElementById('info').innerHTML =
+      '<iframe seamless src="' + url + '" width="600" height="200"></iframe>';
+}
+
+function onSingleClickPrintInformations(map, view, wmsSource) {
+  map.on('singleclick', function(evt) {
+    document.getElementById('info').innerHTML = '';
+    var viewResolution = /** @type {number} */ (view.getResolution());
+
+    var url = getRequestUrl(wmsSource, evt, viewResolution);
+
+    if (url) {
+      if (INFO_FORMAT == JSON_FORMAT) {
+        getAndPrintJSONData(url);
+      } else if (INFO_FORMAT == HTML_FORMAT || INFO_FORMAT == TEXT_FORMAT) {
+        getAndPrintTextData(url);
+      }
+    }
+  });
+}
